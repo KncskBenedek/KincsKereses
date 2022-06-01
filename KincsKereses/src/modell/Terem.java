748f70f7d6a8b3c5cfbcba651b77cc69dda7b719
KncskBenedek <koncsikbenedek@gmail.com>
2022-06-01 16:50:20 +0200

    Terem class

---
+++ KincsKereses/src/modell/Terem.java
@@ -0,0 +1,20 @@
+
+package modell;
+
+
+public class Terem {
+    private int ferohely;
+    private KincsesLada[] KincsesLadak;
+    private boolean megtalalva;
+    private String leiras;
+    
+    public Terem(){
+        
+    }
+    public boolean bekeres(int poz){
+        return false;
+    }
+    public void jatek(){
+        
+    }
+}
