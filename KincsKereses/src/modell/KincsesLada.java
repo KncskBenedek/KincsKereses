83b4b8ff7b841aa4073075c29405b7e17d52e3af
KncskBenedek <koncsikbenedek@gmail.com>
2022-06-01 16:44:08 +0200

    Lada, KincsesLada alap állapot

--- KincsKereses/src/modell/KincsesLada.java
+++ KincsKereses/src/modell/KincsesLada.java
@@ -3,5 +3,14 @@ package modell;
 
 
 public class KincsesLada {
-    
+    String felirat;
+
+    public KincsesLada(Tipus tipus, String[] tartalom, String felirat) {
+      
+    }
+
+   
+    public KincsesLada(Tipus tipus, String felirat){
+        
+    }
 }
--- KincsKereses/src/modell/Lada.java
+++ KincsKereses/src/modell/Lada.java
@@ -1,7 +1,23 @@
-
 package modell;
 
-
 public class Lada {
-    
+
+    private Tipus tipus;
+    private String[] tartalom;
+
+    public Lada() {
+
+    }
+
+    protected Lada(Tipus tipus) {
+
+    }
+
+    public Lada(String[] tartalom) {
+
+    }
+
+    protected Lada(Tipus tipus, String[] tartalom) {
+
+    }
 }
