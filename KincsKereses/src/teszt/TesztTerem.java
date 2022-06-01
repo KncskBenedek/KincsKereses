f59fd999f4eba2ed341a898fe92476efba0bbc57
KncskBenedek <koncsikbenedek@gmail.com>
2022-06-01 18:18:00 +0200

    Teszt isMegtalalva() mellé eset átmentmegbukott

--- KincsKereses/src/teszt/TesztTerem.java
+++ KincsKereses/src/teszt/TesztTerem.java
@@ -9,6 +9,8 @@ public class TesztTerem {
     }
     TesztTerem(){
         isMegtalalvaEltalal();
+        System.out.println("--------------------");
+        isMegtalalvaMelle();
     }
     public void isMegtalalvaEltalal() {
         System.out.println("isMegtalalva eltalál eset");
@@ -16,5 +18,12 @@ public class TesztTerem {
         boolean eredmeny = tesztTerem.isMegtalalva(2);
         assert eredmeny == true : "isMegtalalva eltalált esetnél megbukott";
         System.out.println("isMegtalalva eltalál eset sikeres");
+    }
+    public void isMegtalalvaMelle() {
+        System.out.println("isMegtalalva mellé eset");
+        Terem tesztTerem = new Terem();
+        boolean eredmeny = tesztTerem.isMegtalalva(1);
+        assert eredmeny == false: "isMegtalalva mellé esetnél megbukott";
+        System.out.println("isMegtalalva mellé eset sikeres");
     }
 }
